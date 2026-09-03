da7290003611bbe427418194bc9767e77186da7d
LofiA15Alt <292876393+LofiA15Alt@users.noreply.github.com>
2026-09-04 00:28:02 +0100

    cpufreq : Enhance boost for better UI responsiveness
    Samsung's default values are too conservative, however we can change
    them here because they are set in the Kernel

--- kernel-5.10/drivers/cpufreq/cpufreq.c
+++ kernel-5.10/drivers/cpufreq/cpufreq.c
@@ -1887,6 +1887,7 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpus_read_lock();
 	if (!has_target() && !cpufreq_driver->suspend)
 		goto suspend;
 
@@ -1906,6 +1907,7 @@ void cpufreq_suspend(void)
 
 suspend:
 	cpufreq_suspended = true;
+	cpus_read_unlock();
 }
 
 /**
