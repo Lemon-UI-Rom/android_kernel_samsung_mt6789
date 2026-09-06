08b92ef43261163be530df155dae008c325ec96c
LofiA15Alt <292876393+LofiA15Alt@users.noreply.github.com>
2026-09-06 20:07:41 +0100

    Revert "kernel: balanced tuning (Part 3)"
    This reverts commit 66b1d31f2bd25a8c6a908c8a6222d6eba7a9e9c8.

--- kernel-5.10/kernel/sched/fair.c
+++ kernel-5.10/kernel/sched/fair.c
@@ -656,14 +656,8 @@ struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 int sched_proc_update_handler(struct ctl_table *table, int write,
 		void *buffer, size_t *lenp, loff_t *ppos)
 {
-	int ret;
-	unsigned int factor;
-
-	if (write && task_is_booster(current))
-		return -EPERM;
-
-	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
-	factor = get_update_sysctl_factor();
+	int ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+	unsigned int factor = get_update_sysctl_factor();
 
 	if (ret || !write)
 		return ret;
