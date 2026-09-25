bf9eb1c5a31b0a34d98d3e58f1450f01a0f67203
LofiA15Alt <292876393+LofiA15Alt@users.noreply.github.com>
2026-09-25 15:56:24 +0100

    gpu : tune overclock mode
    Signed-off-by: LofiA15Alt <galaxya15hyper@gmail.com>

--- kernel-5.10/drivers/gpu/mediatek/gpufreq/v2/include/gpufreq_mt6789.h
+++ kernel-5.10/drivers/gpu/mediatek/gpufreq/v2/include/gpufreq_mt6789.h
@@ -287,7 +287,7 @@ struct gpufreq_status {
 
 #define SIGNED_OPP_GPU_NUM              ARRAY_SIZE(g_default_gpu)
 struct gpufreq_opp_info g_default_gpu[] = {
-	GPUOP(1209000, 90000, 90000, POSDIV_POWER_2, 1875, 0), /*  0 OC: 1209MHz @ 900mV (AVS off, validated live) */
+	GPUOP(1212000, 90000, 90000, POSDIV_POWER_2, 1875, 0), /*  0 OC: 1212MHz @ 900mV (AVS off, validated live) */
 	GPUOP(1086000, 89375, 89375, POSDIV_POWER_2, 1875, 0), /*  1 */
 	GPUOP(1072000, 88750, 88750, POSDIV_POWER_2, 1875, 0), /*  2 */
 	GPUOP(1058000, 88125, 88125, POSDIV_POWER_2, 1875, 0), /*  3 */
